0b1917baf50671d4213b604a051288829ccd151e
Cristian Morales <cr15moresp@gmail.com>
2021-09-29 15:00:13 -0500

    Componente para proteger las rutas privadas creado

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,14 +7,19 @@ import {
 
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { AuthContext } from '../auth/AuthContext';
 
 export const AppRouter = () => {
+
+    const { user } = useContext(AuthContext);
+
     return (
         <Router>
             <div>
                 <Switch>
                     <Route exact path="/login" component={ LoginScreen } />,
-                    <Route path="/" component={ DashboardRoutes } />
+                    <PrivateRoute isAuthenticated={user.logged} path="/" component={ DashboardRoutes } />
                 </Switch>
             </div>
         </Router>
