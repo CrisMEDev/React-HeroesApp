2284540ca90296c9a7717e9b3baef5edbd05d830
Cristian Morales <cr15moresp@gmail.com>
2021-09-30 00:15:17 -0500

    Pruebas en el componente DashboardRoutes realizadas

---
+++ src/tests/routers/DashboardRoutes.test.js
@@ -0,0 +1,32 @@
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import { AuthContext } from '../../auth/AuthContext';
+import { DashboardRoutes } from '../../routers/DashboardRoutes';
+
+describe('Pruebas en el componente <DashboardRoutes />', () => {
+    
+    const contextValue = {
+        user: {
+            logged: true,
+            name: 'Cristian'
+        },
+        dispatch: jest.fn()
+    }
+    
+    test('Debe mostrarse correctamente', () => {
+        
+        const wrapper = mount(
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter>
+                    <DashboardRoutes />
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('.text-info').text().trim() ).toBe('Cristian');
+        
+    });
+    
+    
+});
